7c4ca44e06a08ef99c133fec56be0925e6d5d645
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "hpc_cache_blob_nfs_targets" {
-  description = "All hpc_cache_blob_nfs_target resources"
-  value       = azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets
-}
 output "hpc_cache_blob_nfs_targets_access_policy_name" {
-  description = "List of access_policy_name values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.access_policy_name]
+  description = "Map of access_policy_name values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.access_policy_name }
 }
 output "hpc_cache_blob_nfs_targets_cache_name" {
-  description = "List of cache_name values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.cache_name]
+  description = "Map of cache_name values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.cache_name }
 }
 output "hpc_cache_blob_nfs_targets_name" {
-  description = "List of name values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.name]
+  description = "Map of name values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.name }
 }
 output "hpc_cache_blob_nfs_targets_namespace_path" {
-  description = "List of namespace_path values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.namespace_path]
+  description = "Map of namespace_path values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.namespace_path }
 }
 output "hpc_cache_blob_nfs_targets_resource_group_name" {
-  description = "List of resource_group_name values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.resource_group_name]
+  description = "Map of resource_group_name values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.resource_group_name }
 }
 output "hpc_cache_blob_nfs_targets_storage_container_id" {
-  description = "List of storage_container_id values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.storage_container_id]
+  description = "Map of storage_container_id values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.storage_container_id }
 }
 output "hpc_cache_blob_nfs_targets_usage_model" {
-  description = "List of usage_model values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.usage_model]
+  description = "Map of usage_model values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.usage_model }
 }
 output "hpc_cache_blob_nfs_targets_verification_timer_in_seconds" {
-  description = "List of verification_timer_in_seconds values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.verification_timer_in_seconds]
+  description = "Map of verification_timer_in_seconds values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.verification_timer_in_seconds }
 }
 output "hpc_cache_blob_nfs_targets_write_back_timer_in_seconds" {
-  description = "List of write_back_timer_in_seconds values across all hpc_cache_blob_nfs_targets"
-  value       = [for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : v.write_back_timer_in_seconds]
+  description = "Map of write_back_timer_in_seconds values across all hpc_cache_blob_nfs_targets, keyed the same as var.hpc_cache_blob_nfs_targets"
+  value       = { for k, v in azurerm_hpc_cache_blob_nfs_target.hpc_cache_blob_nfs_targets : k => v.write_back_timer_in_seconds }
 }
 
